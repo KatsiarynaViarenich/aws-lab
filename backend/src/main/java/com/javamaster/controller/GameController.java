72ffda8d4ffe0286bc40edf85d51a166cbc4cc29
katsiaryna <katsiaryna.viarenich@pwr.edu.pl>
2024-04-21 13:19:21 +0200

    Initial game with docker files

---
+++ backend/src/main/java/com/javamaster/controller/GameController.java
@@ -0,0 +1,55 @@
+package com.javamaster.controller;
+
+import com.javamaster.controller.dto.ConnectRequest;
+import com.javamaster.exception.InvalidGameException;
+import com.javamaster.exception.InvalidParamException;
+import com.javamaster.exception.NotFoundException;
+import com.javamaster.model.Game;
+import com.javamaster.model.GamePlay;
+import com.javamaster.model.Player;
+import com.javamaster.service.GameService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+@AllArgsConstructor
+@RequestMapping("/game")
+public class GameController {
+
+    private final GameService gameService;
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    @CrossOrigin(origins = "http://localhost:8081")
+    @PostMapping("/start")
+    public ResponseEntity<Game> start(@RequestBody Player player) {
+        log.info("start game request: {}", player);
+        return ResponseEntity.ok(gameService.createGame(player));
+    }
+
+    @CrossOrigin(origins = "http://localhost:8081")
+    @PostMapping("/connect")
+    public ResponseEntity<Game> connect(@RequestBody ConnectRequest request) throws InvalidParamException, InvalidGameException {
+        log.info("connect request: {}", request);
+        return ResponseEntity.ok(gameService.connectToGame(request.getPlayer(), request.getGameId()));
+    }
+
+    @CrossOrigin(origins = "http://localhost:8081")
+    @PostMapping("/connect/random")
+    public ResponseEntity<Game> connectRandom(@RequestBody Player player) throws NotFoundException {
+        log.info("connect random {}", player);
+        return ResponseEntity.ok(gameService.connectToRandomGame(player));
+    }
+
+    @CrossOrigin(origins = "http://localhost:8081")
+    @PostMapping("/gameplay")
+    public ResponseEntity<Game> gamePlay(@RequestBody GamePlay request) throws NotFoundException, InvalidGameException {
+        log.info("gameplay: {}", request);
+        Game game = gameService.gamePlay(request);
+        simpMessagingTemplate.convertAndSend("/topic/game-progress/" + game.getGameId(), game);
+        return ResponseEntity.ok(game);
+    }
+}
